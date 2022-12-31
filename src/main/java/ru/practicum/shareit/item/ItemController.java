2dcc4ab2c25f1855f9f938181cbfba1eaca11d83
Константин Михалев <mikhalevks@yandex.ru>
2022-12-31 12:57:51 +0300

    refactor: логирование в контроллерах user и item

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.ItemDto;
 
@@ -8,41 +9,56 @@ import javax.validation.Valid;
 import java.util.Collection;
 
 @RestController
+@Slf4j
 @RequestMapping("/items")
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemService itemService;
+    private static final String COMMON_ITEM_PATH = "/items";
+    private static final String ITEM_PREFIX = "{itemId}";
+    private static final String SEARCH_PREFIX = "/search";
+    private static final String USER_REQUEST_HEADER = "X-Sharer-User-Id";
 
-    @GetMapping("{itemId}")
+    @GetMapping(ITEM_PREFIX)
     public ItemDto getItemById(@PathVariable long itemId) {
+        log.info("Получен запрос GET к эндпоинту: " + COMMON_ITEM_PATH + "/" + itemId);
         return itemService.findById(itemId);
     }
 
     @GetMapping
-    public Collection<ItemDto> getAllByOwnerId(@RequestHeader("X-Sharer-User-Id") long ownerId) {
+    public Collection<ItemDto> getAllByOwnerId(@RequestHeader(USER_REQUEST_HEADER) long ownerId) {
+        log.info("Получен запрос GET к эндпоинту: " + COMMON_ITEM_PATH + " от пользователя с id = " + ownerId);
         return itemService.findAllByOwnerId(ownerId);
     }
 
     @PostMapping
-    public ItemDto create(@RequestHeader("X-Sharer-User-Id") long userId, @Valid @RequestBody ItemDto itemDto) {
+    public ItemDto create(@RequestHeader(USER_REQUEST_HEADER) long userId, @Valid @RequestBody ItemDto itemDto) {
+        log.info("Получен запрос POST к эндпоинту: " + COMMON_ITEM_PATH + " от пользователя с id = " + userId +
+                ". Данные тела запроса: {}", itemDto);
         return itemService.create(itemDto, userId);
     }
 
-    @PatchMapping("/{itemId}")
-    public ItemDto update(@RequestHeader("X-Sharer-User-Id") long userId,
+    @PatchMapping(ITEM_PREFIX)
+    public ItemDto update(@RequestHeader(USER_REQUEST_HEADER) long userId,
                           @PathVariable Long itemId,
                           @RequestBody ItemDto itemDto) {
+        log.info("Получен запрос PATCH к эндпоинту: " + COMMON_ITEM_PATH + "/" + itemId +
+                " от пользователя с id = " + userId + ". Данные тела запроса: {}", itemDto);
         return itemService.update(itemDto, itemId, userId);
     }
 
-    @DeleteMapping("{itemId}")
-    public void delete(@RequestHeader("X-Sharer-User-Id") long userId,
+    @DeleteMapping(ITEM_PREFIX)
+    public void delete(@RequestHeader(USER_REQUEST_HEADER) long userId,
                        @PathVariable long itemId) {
+        log.info("Получен запрос DELETE к эндпоинту: " + COMMON_ITEM_PATH + "/" + itemId +
+                " от пользователя с id = " + userId);
         itemService.delete(itemId, userId);
     }
 
-    @GetMapping("/search")
+    @GetMapping(SEARCH_PREFIX)
     public Collection<ItemDto> getAvailableByText(@RequestParam String text) {
+        log.info("Получен запрос GET к эндпоинту: " + COMMON_ITEM_PATH + SEARCH_PREFIX +
+                ". Строка поиска: {}", text);
         return itemService.findAvailableByText(text);
     }
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDto;
 
@@ -8,33 +9,43 @@ import javax.validation.Valid;
 import java.util.Collection;
 
 @RestController
+@Slf4j
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private static final String COMMON_USER_PATH = "/users";
+    private static final String USER_PREFIX = "{userId}";
 
-    @GetMapping("{userId}")
+
+    @GetMapping(USER_PREFIX)
     public UserDto getUserById(@PathVariable long userId) {
+        log.info("Получен запрос GET к эндпоинту: " + COMMON_USER_PATH + "/" + userId);
         return userService.findById(userId);
     }
 
     @GetMapping
     public Collection<UserDto> getAllUsers() {
+        log.info("Получен запрос GET к эндпоинту: " + COMMON_USER_PATH);
         return userService.findAll();
     }
 
     @PostMapping
     public UserDto create(@Valid @RequestBody UserDto userDto) {
+        log.info("Получен запрос POST к эндпоинту: " + COMMON_USER_PATH + ". Данные тела запроса: {}", userDto);
         return userService.create(userDto);
     }
 
-    @PatchMapping("/{userId}")
+    @PatchMapping(USER_PREFIX)
     public UserDto update(@RequestBody UserDto userDto, @PathVariable Long userId) {
+        log.info("Получен запрос PATCH к эндпоинту: " + COMMON_USER_PATH + "/" + userId +
+                ". Данные тела запроса: {}", userDto);
         return userService.update(userId, userDto);
     }
 
-    @DeleteMapping("{userId}")
+    @DeleteMapping(USER_PREFIX)
     public void delete(@PathVariable long userId) {
+        log.info("Получен запрос DELETE к эндпоинту: " + COMMON_USER_PATH + "/" + userId);
         userService.delete(userId);
     }
 }
