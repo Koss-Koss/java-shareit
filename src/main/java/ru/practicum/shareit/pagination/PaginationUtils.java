73552f6fdca72b8c55707c4ccef007562624943c
Константин Михалев <mikhalevks@yandex.ru>
2023-02-05 18:05:48 +0300

    refactor: add docker with empty server

--- src/main/java/ru/practicum/shareit/pagination/PaginationUtils.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.pagination;
-
-public class PaginationUtils {
-    public static int getCalculatedPage(long from, int size) {
-
-        return Math.toIntExact(from / size);
-    }
-}
